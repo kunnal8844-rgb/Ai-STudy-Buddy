92d179e49b1087fd882951a0e4ab3dd98240226a
kunnal8844-rgb <kunnal8844@gmail.com>
2026-06-11 11:46:51 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import google.generativeai as genai
+
+# Put your Gemini API Key here
+genai.configure(api_key="AQ.Ab8RN6Iu2bnpGEsQ3DfDSKdLMIDL8ST-KlpDF6_48JHK33PjYg")
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    answer = ""
+
+    if request.method == "POST":
+        question = request.form["question"]
+
+        try:
+            response = model.generate_content(
+                f"Explain this topic in simple student-friendly language:\n{question}"
+            )
+            answer = response.text
+
+        except Exception as e:
+            answer = f"Error: {e}"
+
+    return render_template("index.html", answer=answer)
+
+if __name__ == "__main__":
+    app.run(debug=True)
